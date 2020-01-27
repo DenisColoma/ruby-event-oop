747f7faaeff8ad66fe40f493275ebb777a6a9416
Denis COLOMA <coloma.denis@gmail.com>
2020-01-27 18:09:52 +0100

    POO_1ere_partie

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,21 @@
-class Event
+require 'pry'
+require 'time'
+require 'jaro_winkler'
 
+class Event
+	attr_accessor :start_date, :duration, :title, :attendees
+
+	def initialize(date_init, duration_init, title_init, attendees_init)
+		@start_date = Time.parse(date_init)
+		@duration = duration_init
+		@title = title_init
+		@attendees = attendees_init
+	end
+
+	def postpone_24h
+		@start_date + 86400	
+	end
 end
+
+binding.pry
+	puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,23 @@
+require 'pry'
+require 'jaro_winkler'
+
 class User
+	attr_accessor :email, :age
+	@@all_users = []
+
+	def initialize(init_email, init_age)
+		@email = init_email
+		@age = init_age
+		@@all_users << self
+
+	end
+
+	def self.all
+		return @@all_users
+		
+	end
 
 end
+
+binding.pry
+	puts "end of file"
